822ca28e5fb06d44e4ee4ab70b118202052e2217
MereacreEugen01 <mereacre.eugen@studenti-ittfedifermi.edu.it>
2022-01-22 20:19:02 +0100

    Implementato il tasto traduci, momentaneamente copia la scritta che si trova nel campo da tradurre e la incolla nel campo del testo tradotto

--- app/src/main/java/com/example/traduttore/MainActivity.java
+++ app/src/main/java/com/example/traduttore/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.traduttore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView testoDaTradurre;
+    private TextView testoTradotto;
+    //private Button traduci = (Button) this.findViewById(R.id.bottoneTraduzione);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.testoDaTradurre = (TextView) findViewById(R.id.testoDaTradurre);
+        this.testoTradotto = (TextView) findViewById(R.id.testoTradotto);
+
+
+
+    }
+    public void onClickTraduci(View arg0)
+    {
+        testoTradotto.setText(testoDaTradurre.getText());
     }
 }
