8b4dda74ac15dfb0f59ec5e0c6cf87f6681d58e7
LenziFilippo04 <lenzi.filippo@studenti-ittfedifermi.edu.it>
2022-02-12 12:03:27 +0100

    Inserimento logica delle impostazioni

--- app/src/main/java/com/example/traduttore/Impostazioni.java
+++ app/src/main/java/com/example/traduttore/Impostazioni.java
@@ -2,7 +2,14 @@ package com.example.traduttore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.net.MalformedURLException;
 
 public class Impostazioni extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class Impostazioni extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_impostazioni);
+
+        SharedPreferences s = getSharedPreferences("settings", MODE_PRIVATE);
+        int a = s.getInt("source", 0);
+        int b = s.getInt("target", 1);
+        String c = s.getString("key", null);
+
+        ((Spinner)findViewById(R.id.linguaPartenza)).setSelection(a);
+        ((Spinner)findViewById(R.id.linguaArrivo)).setSelection(b);
+        ((EditText)findViewById(R.id.key)).setText(c);
+
+
+
+    }
+    public void onClickSave(View arg0) {
+        int source = ((Spinner)findViewById(R.id.linguaPartenza)).getSelectedItemPosition();
+        int target = ((Spinner)findViewById(R.id.linguaArrivo)).getSelectedItemPosition();
+        String key = ((EditText)findViewById(R.id.key)).getText().toString();
+        SharedPreferences.Editor e = getSharedPreferences("settings", MODE_PRIVATE).edit();
+        e.putInt("source", source);
+        e.putInt("target", target);
+        e.putString("key", key);
+        e.commit();
     }
 }
