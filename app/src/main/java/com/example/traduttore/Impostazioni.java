4573c264fcae83a5e352b220b3374b87e88b017d
MereacreEugen01 <mereacre.eugen@studenti-ittfedifermi.edu.it>
2022-02-16 12:55:32 +0100

    Activity impistazioni completata, Continuare Inserimento lingue, complitare sviluppo logica generale e create UI DECENTE

--- app/src/main/java/com/example/traduttore/Impostazioni.java
+++ app/src/main/java/com/example/traduttore/Impostazioni.java
@@ -2,8 +2,10 @@ package com.example.traduttore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -12,25 +14,34 @@ import android.widget.Spinner;
 import java.net.MalformedURLException;
 
 public class Impostazioni extends AppCompatActivity {
+    private long mLastClickTime = 0;
+    private Spinner sceltaLinguaPartenza;
+    private Spinner sceltaLinguaArrivo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_impostazioni);
+        this.sceltaLinguaPartenza = (Spinner) findViewById((R.id.linguaPartenza));
+        this.sceltaLinguaArrivo = (Spinner) findViewById((R.id.linguaArrivo));
 
         SharedPreferences s = getSharedPreferences("settings", MODE_PRIVATE);
         int a = s.getInt("source", 0);
         int b = s.getInt("target", 1);
-        String c = s.getString("key", null);
-
+        String c = s.getString("key", "d17b9f49-8ee1-06a3-161a-8cb2a513ae10:fx");
         ((Spinner)findViewById(R.id.linguaPartenza)).setSelection(a);
         ((Spinner)findViewById(R.id.linguaArrivo)).setSelection(b);
         ((EditText)findViewById(R.id.key)).setText(c);
+        //((EditText)findViewById(R.id.key)).setHint(c);
 
 
 
     }
     public void onClickSave(View arg0) {
+        if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) {
+            return;
+        }
+        mLastClickTime = SystemClock.elapsedRealtime();
         int source = ((Spinner)findViewById(R.id.linguaPartenza)).getSelectedItemPosition();
         int target = ((Spinner)findViewById(R.id.linguaArrivo)).getSelectedItemPosition();
         String key = ((EditText)findViewById(R.id.key)).getText().toString();
@@ -39,5 +50,16 @@ public class Impostazioni extends AppCompatActivity {
         e.putInt("target", target);
         e.putString("key", key);
         e.commit();
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
+    public void onClickScambia(View arg0)
+    {
+        int sceltaUno = sceltaLinguaPartenza.getSelectedItemPosition();
+        int sceltaDue = sceltaLinguaArrivo.getSelectedItemPosition();
+        sceltaLinguaPartenza.setSelection(sceltaDue+1);
+        sceltaLinguaArrivo.setSelection(sceltaUno-1);
+    }
+
+
 }
--- app/src/main/java/com/example/traduttore/MainActivity.java
+++ app/src/main/java/com/example/traduttore/MainActivity.java
@@ -2,15 +2,14 @@ package com.example.traduttore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.SyncStateContract;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.io.UnsupportedEncodingException;
@@ -24,7 +23,6 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -34,6 +32,7 @@ public class MainActivity extends AppCompatActivity
     private Spinner sceltaLinguaPartenza;
     private Spinner sceltaLinguaArrivo;
     private ListaLingue convertitore = new ListaLingue();
+    private long mLastClickTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +43,13 @@ public class MainActivity extends AppCompatActivity
         this.sceltaLinguaPartenza = (Spinner) findViewById((R.id.linguaPartenza));
         this.sceltaLinguaArrivo = (Spinner) findViewById((R.id.linguaArrivo));
 
+        SharedPreferences s = getSharedPreferences("settings", MODE_PRIVATE);
+        int a = s.getInt("source", 0);
+        int b = s.getInt("target", 1);
+        String c = s.getString("key", "d17b9f49-8ee1-06a3-161a-8cb2a513ae10:fx");
 
-
-
+        ((Spinner)findViewById(R.id.linguaPartenza)).setSelection(a);
+        ((Spinner)findViewById(R.id.linguaArrivo)).setSelection(b);
 
 
     }
@@ -54,6 +57,9 @@ public class MainActivity extends AppCompatActivity
         //testoTradotto.setText(testoDaTradurre.getText());
         //testoDaTradurre.setHint("Testo Da tradurre:");
         // ;
+        SharedPreferences s = getSharedPreferences("settings", MODE_PRIVATE);
+        String c = s.getString("key", "d17b9f49-8ee1-06a3-161a-8cb2a513ae10:fx");
+
 
         Log.d("decode", encodeValue("Ciao come stai"));
         Log.d("config","Lingua inserita: "+ sceltaLinguaPartenza.getSelectedItem().toString() + ": "+
@@ -70,9 +76,28 @@ public class MainActivity extends AppCompatActivity
         String encodedQuery = encodeValue(prova);
         System.out.println(encodedQuery);
 
-        inviaRichiesta(encodeValue(prova), convertitore.getLinguaScelta(sceltaLinguaArrivo.getSelectedItem().toString()), convertitore.getLinguaScelta(sceltaLinguaPartenza.getSelectedItem().toString()));
+        inviaRichiesta(encodeValue(prova), convertitore.getLinguaScelta(sceltaLinguaArrivo.getSelectedItem().toString()), convertitore.getLinguaScelta(sceltaLinguaPartenza.getSelectedItem().toString()), c);
 
     }
+
+    public void onClickImposta(View arg0)
+    {
+        if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) {
+            return;
+        }
+        mLastClickTime = SystemClock.elapsedRealtime();
+        Intent i = new Intent(this, Impostazioni.class);
+        startActivity(i);
+    }
+    public void onClickScambia(View arg0)
+    {
+        int sceltaUno = sceltaLinguaPartenza.getSelectedItemPosition();
+        int sceltaDue = sceltaLinguaArrivo.getSelectedItemPosition();
+        sceltaLinguaPartenza.setSelection(sceltaDue+1);
+        sceltaLinguaArrivo.setSelection(sceltaUno-1);
+    }
+
+
     private static String encodeValue(String value)
     {
         try {
@@ -92,19 +117,20 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    private void inviaRichiesta(String testoDaTradurre, String target_lang, String source_lang)
+    private void inviaRichiesta(String testoDaTradurre, String target_lang, String source_lang, String auth_key)
     {
         String e = "&";
         String testo = "text="+testoDaTradurre;
         String target = "target_lang="+target_lang;
         String sorgente = "";
+        String chiave = "auth_key="+auth_key;
         if(source_lang == null) { }
         else
         {
             sorgente = "source_lang="+source_lang;
         }
-        String url = "https://api-free.deepl.com/v2/translate?auth_key=d17b9f49-8ee1-06a3-161a-8cb2a513ae10:fx"+e+testo+e+target+e+sorgente;
-
+        String url = "https://api-free.deepl.com/v2/translate?"+chiave+e+testo+e+target+e+sorgente;
+        Log.d("richista", url);
         //definisco la coda delle richieste
         RequestQueue queue = Volley.newRequestQueue(this);
         System.out.println(url);
@@ -121,7 +147,6 @@ public class MainActivity extends AppCompatActivity
                     {
                         Log.d("API", response);
                         String traduzione = response.substring(response.indexOf("\"text\":\"")+8, response.indexOf("\"}]}"));
-                        //String t = null;
                         testoTradotto.setText(decodeValue(traduzione));
                     }
                 },
@@ -130,7 +155,7 @@ public class MainActivity extends AppCompatActivity
                     @Override
                     public void onErrorResponse(VolleyError error)
                     {
-                        //System.out.println("c'è stato un errore");
+                        testoTradotto.setText("Si è verificato un errore, si prega di riprovare!");
                         Log.e("API",error.getLocalizedMessage());
                     }
                 }
